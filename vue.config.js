73f7766b489021e594b1462411f07090a2fdb4df
Dmitry Lartsev <dmitry.lartsev@noveogroup.com>
2020-06-23 23:25:57 +0300

    Add travis deploy

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,8 @@
+const REPO_NAME = 'dogs-vue' 
+const publicPath = process.env.NODE_ENV === 'production' ? REPO_NAME : '/'
+
 module.exports = {
+  publicPath,
   chainWebpack: config => {
     config.module
       .rule('svg-sprite')
